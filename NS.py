356cec53a88f03be858433c515b5c5b88285067a
Thomas <thomas68allison@hotmail.com>
2018-01-07 23:23:00 +0100

    NS api

--- NS.py
+++ NS.py
@@ -1,4 +1,41 @@
-# from tkinter import *
-#
-# class NS(Frame):
-#
+from tkinter import *
+import requests
+from requests.auth import HTTPBasicAuth
+import xml.etree.cElementTree as ET
+
+ns_credentials_user = "thomas68allison@hotmail.com"
+ns_credentials_password = "8ozlr3V3JfyM-_eLfwUEpLGp12AX9rCds0DoPnhmIAmCtlIedaS_Cw"
+
+
+class NSFrame(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent, bg='black')
+        print(self.get_ns_departure())
+
+
+def get_all_trains():
+    xml = get_ns_departure()
+
+    print(xml.tag)
+
+    for train_info in xml:
+        # print(train_info)
+        print("=========")
+        for train in train_info:
+            print(vars(train))
+
+
+
+def get_ns_departure():
+    url = 'https://webservices.ns.nl/ns-api-avt?station=LEDN'
+
+    response = requests.get(url, auth=get_ns_credentials(), stream=True)
+
+    return ET.fromstring(response.content)
+
+
+def get_ns_credentials():
+    return HTTPBasicAuth(ns_credentials_user, ns_credentials_password)
+
+
+get_all_trains()
