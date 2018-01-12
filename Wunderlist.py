318d08ec7c2e1c48dc43488d29e1a8df9199e9ac
Thomas <thomas68allison@hotmail.com>
2018-01-12 18:04:18 +0100

    Working Wunderlist

--- Wunderlist.py
+++ Wunderlist.py
@@ -1,28 +1,58 @@
-from random import randint
-
+import json
+import traceback
+from tkinter import *
 import requests
 
+from settings import medium_text_size, small_text_size
+
+REFRESH_RATE = 1200000
+
 ACCESS_TOKEN = "332c08cb64ad807d265bea88765f20ec6c843f81882aa57228b60ee0ca49"
-
 CLIENT_ID = "8ea9ab3c56a5a558688a"
-CODE = "ae846cb0400eda557ec8"
-REDIRECT_URL = "https://github.com/tmallison/magicmirror"
-CLIENT_SECRET = "3f8e473845a59adf33cb49c2a726cb65055fcb601707452a3848f1d1d5ff"
+HOME_LIST_ID = 327759681
+HEAD = {'X-Access-Token': ACCESS_TOKEN,
+        'X-Client-ID': CLIENT_ID}
 
-url = "https://www.wunderlist.com/oauth/authorize?client_id={}&redirect_uri={}&state={}".format(CLIENT_ID,
-                                                                                                REDIRECT_URL,
-                                                                                                randint(1000, 10000))
-print(url)
-# response = requests.get(url)
-# print(vars(response))
 
-def get_list():
-    url = "http://a.wunderlist.com/api/v1/lists"
-    head = {'X-Access-Token': ACCESS_TOKEN,
-            'X-Client-ID': CLIENT_ID}
+class TaskFrame(Frame):
+    def __init__(self, parent, text, text_size=small_text_size):
+        Frame.__init__(self, parent, bg='black')
+        self.task_lbl = Label(self, text="- {}".format(text), font=('Helvetica', text_size), fg="white", bg="black")
+        self.task_lbl.pack(side=LEFT, anchor=N)
 
-    response = requests.get(url, headers=head)
 
-    return response.content
+class WunderlistFrame(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.config(bg='black')
+        self.title = 'Wunderlist'
+        self.wunderlist_lbl = Label(self, text=self.title, font=('Helvetica', medium_text_size), fg="white", bg="black")
+        self.wunderlist_lbl.pack(side=TOP, anchor=W)
+        self.wunderlist_container = Frame(self, bg="black")
+        self.wunderlist_container.pack(side=TOP)
+        self.display_all_tasks()
 
-print(get_list())
+    def display_all_tasks(self):
+        try:
+            # remove all children
+            for widget in self.wunderlist_container.winfo_children():
+                widget.destroy()
+
+            tasks = self.get_tasks_in_list(HOME_LIST_ID)
+
+            for task in tasks[0:5]:
+                task_frame = TaskFrame(self.wunderlist_container, task)
+                task_frame.pack(side=TOP, anchor=W)
+
+        except Exception as e:
+            traceback.print_exc()
+            print("Error: %s. Cannot get to Wunderlist." % e)
+
+        self.after(REFRESH_RATE, self.display_all_tasks)
+
+    def get_tasks_in_list(self, list_id):
+        url = "http://a.wunderlist.com/api/v1/tasks"
+        response = requests.get(url, headers=HEAD, params={"list_id": list_id})
+        tasks = json.loads(response.text)
+
+        return [t["title"] for t in tasks]
--- main.py
+++ main.py
@@ -1,53 +1,8 @@
-import traceback
 from tkinter import *
-import feedparser
 
 from Clock import ClockFrame
 from NS import NSFrame
-from settings import small_text_size, medium_text_size, news_country_code
-
-
-class NewsHeadline(Frame):
-    def __init__(self, parent, event_name=""):
-        Frame.__init__(self, parent, bg='black')
-
-        self.eventName = event_name
-        self.eventNameLbl = Label(self, text=self.eventName, font=('Helvetica', small_text_size), fg="white", bg="black")
-        self.eventNameLbl.pack(side=LEFT, anchor=N)
-
-
-class News(Frame):
-    def __init__(self, parent, *args, **kwargs):
-        Frame.__init__(self, parent, *args, **kwargs)
-        self.config(bg='black')
-        self.title = 'News' # 'News' is more internationally generic
-        self.newsLbl = Label(self, text=self.title, font=('Helvetica', medium_text_size), fg="white", bg="black")
-        self.newsLbl.pack(side=TOP, anchor=W)
-        self.headlinesContainer = Frame(self, bg="black")
-        self.headlinesContainer.pack(side=TOP)
-        self.get_headlines()
-
-    def get_headlines(self):
-        try:
-            # remove all children
-            for widget in self.headlinesContainer.winfo_children():
-                widget.destroy()
-            if news_country_code == None:
-                headlines_url = "https://news.google.com/?output=rss"
-            else:
-                headlines_url = "https://news.google.com/news?ned=%s&output=rss" % news_country_code
-
-            feed = feedparser.parse(headlines_url)
-
-            for post in feed.entries[0:5]:
-                headline = NewsHeadline(self.headlinesContainer, post.title)
-                headline.pack(side=TOP, anchor=W)
-        except Exception as e:
-            traceback.print_exc()
-            print("Error: %s. Cannot get news." % e)
-
-        self.after(600000, self.get_headlines)
-
+from Wunderlist import WunderlistFrame
 
 class FullscreenWindow:
     def __init__(self):
@@ -55,23 +10,23 @@ class FullscreenWindow:
         self.tk.configure(background='black')
         self.topFrame = Frame(self.tk, background='black')
         self.bottomFrame = Frame(self.tk, background='black')
-        self.topFrame.pack(side = TOP, fill=BOTH, expand=YES)
-        self.bottomFrame.pack(side = BOTTOM, fill=BOTH, expand=YES)
+        self.topFrame.pack(side=TOP, fill=BOTH, expand=YES)
+        self.bottomFrame.pack(side=BOTTOM, fill=BOTH, expand=YES)
         self.state = False
         self.tk.bind("<Return>", self.toggle_fullscreen)
         self.tk.bind("<Escape>", self.end_fullscreen)
+
         # clock
         self.clock = ClockFrame(self.topFrame)
         self.clock.pack(side=RIGHT, anchor=N, padx=100, pady=60)
-        # # weather
-        # self.weather = Weather(self.topFrame)
-        # self.weather.pack(side=LEFT, anchor=N, padx=100, pady=60)
-        # # news
+
+        # wunderlist
+        self.wunderlist = WunderlistFrame(self.topFrame)
+        self.wunderlist.pack(side=LEFT, anchor=N, padx=100, pady=60)
+
+        # NS
         self.ns = NSFrame(self.bottomFrame)
         self.ns.pack(side=LEFT, anchor=S, padx=100, pady=60)
-        # calender - removing for now
-        # self.calender = Calendar(self.bottomFrame)
-        # self.calender.pack(side = RIGHT, anchor=S, padx=100, pady=60)
 
     def toggle_fullscreen(self, event=None):
         self.state = not self.state  # Just toggling the boolean
